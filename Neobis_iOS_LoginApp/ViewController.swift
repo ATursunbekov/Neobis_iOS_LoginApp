8d32b8302e301ea7e8d8ecb16d6a974c7bc7a728
Alikhan <alikhan.tursunbekov@gmail.com>
2023-10-23 21:10:33 +0600

    Last part

--- Neobis_iOS_LoginApp/ViewController.swift
+++ Neobis_iOS_LoginApp/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    @IBAction func signIn(_ sender: Any) {
+        if username != nil && password != nil {
+            print("Username: \(username.text!), password: \(password.text!)")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
